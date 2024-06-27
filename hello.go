7617248b4b7d65e0cd3c5d5de50805df042a6b54
Semir Deljic <smd@pomfort.com>
2024-06-28 00:27:42 +0200

    added hi function

--- hello.go
+++ hello.go
@@ -3,6 +3,11 @@ package hello
 import "fmt"
 
 func Hello(name string) string {
+	message := fmt.Sprintf("Hello, %v. Welcome !", name)
+	return message
+}
+
+func Hi(name string) string {
 	message := fmt.Sprintf("Hi, %v. Welcome !", name)
 	return message
 }
